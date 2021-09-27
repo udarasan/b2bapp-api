c6dc914db183e5160cb8a6c17651892bf8925896
udarasan <udarassanjeewa@gmail.com>
2021-09-27 11:17:36 +0530

    Initial commit

---
+++ src/main/java/lk/nsbm/b2bappapi/B2bappApiApplication.java
@@ -0,0 +1,13 @@
+package lk.nsbm.b2bappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2bappApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(B2bappApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/nsbm/b2bappapi/B2bappApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.nsbm.b2bappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2bappApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
